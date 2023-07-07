3f748477f81d365e5cb7ef771b9db05d86b4ff83
hm <gjals00@gmail.com>
2023-07-07 16:12:56 +0900

    commit

--- google_bard_example.py
+++ google_bard_example.py
@@ -2,10 +2,16 @@ from bardapi import Bard
 import os
 from streamlit_chat import message
 import streamlit as st
+import pandas as pd
 
 os.environ['_BARD_API_KEY'] = "YQh7pcL2ubupO5q8lUsrlbkkyzKNBcEAPc_7dPzYvep67HzN22yFKrloub0OrIyxAewlAQ."
 
 st.title("My ChatBot")
+
+if 'generate' not in st.session_state:
+    st.session_state.generate = []
+if 'past' not in st.session_state:
+    st.session_state.past = []
 
 def response_api(prompt):
     message = Bard().get_answer(str(prompt))['content']
@@ -15,11 +21,6 @@ def user_input():
     input_text = st.text_input("Enter Your Prompt:")
     return input_text
 
-if 'generate' not in st.session_state:
-    st.session_state['generate'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
 user_text = user_input()
 
 if user_text:
@@ -27,8 +28,12 @@ if user_text:
     st.session_state.generate.append(output)
     st.session_state.past.append(user_text)
 
-if st.session_state['generate']:
+if st.session_state.generate:
 
-    for i in range(len(st.session_state['generate']) -1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state['generate'][i], key=str(i))
+    for i in range(len(st.session_state.generate) -1, -1, -1):
+        message(st.session_state.past[i], is_user=True, key=str(i) + '_user')
+        message(st.session_state.generate[i], key=str(i))
+
+
+
+
